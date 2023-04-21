3d547bc208315c4ce25f9d245a5cecfe1fc264a3
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-04-21 04:09:52 +0000

    After 3 hours of frustration I've done it

--- practice.rb
+++ practice.rb
@@ -28,58 +28,66 @@ blockchain = [
 
 # 👇👇👇 Your code HERE 👇👇👇
 
-# Setting up hash variables for loop
-hash_index = 0
-hash = blockchain[hash_index]
-
 # Setting up initial wallet values for each person
-ben_inflow = 0
-ben_outflow = 0
-brian_inflow = 0
-brian_outflow = 0
-evan_inflow = 0
-evan_outflow = 0
-anthony_inflow = 0
-anthony_outflow = 0
+ben_total = 0
+brian_total = 0
+evan_total = 0
+anthony_total = 0
 
-# Loop to sum up wallet inflow
-for hash in blockchain
-  if hash["to_user"] = "ben"
-    ben_inflow = ben_inflow + hash["amount"]
-  elsif hash["to_user"] = "brian"
-    brian_inflow = brian_inflow + hash["amount"]
-  elsif hash["to_user"] = "evan"
-    evan_inflow = evan_inflow + hash["amount"]
-  elsif hash["from_user"] = "evan"
-    anthony_inflow = anthony_inflow + hash["amount"]
+# Setting up hash variables for loop
+blockchain_index = 0
+blockchain_entry = blockchain[blockchain_index]
+
+# Loop calculating additions to wallet
+loop do 
+  
+  # Stop loop once we've gone through all the hashes
+  if blockchain_index == blockchain.count
+    break
   end
+
+  case
+    when blockchain[blockchain_index]["to_user"] == "ben"
+      ben_total = ben_total + blockchain[blockchain_index]["amount"]
+    when blockchain[blockchain_index]["to_user"] == "brian"
+      brian_total = brian_total + blockchain[blockchain_index]["amount"]
+    when blockchain[blockchain_index]["to_user"] == "evan"
+      evan_total= evan_total + blockchain[blockchain_index]["amount"]
+    when blockchain[blockchain_index]["to_user"] == "anthony"
+      anthony_total = anthony_total + blockchain[blockchain_index]["amount"]
+  end
+
+  blockchain_index = blockchain_index + 1
+
 end
 
-puts "Inflow:"
-puts ben_inflow
-puts brian_inflow
-puts evan_inflow
-puts anthony_inflow
+# Reset blockchain_index to 0 for use in next loop (is this bad practice?)
+blockchain_index = 0
 
-# Loop to withdraw wallet outflow
-for hash in blockchain
-  if hash["from_user"] = "ben"
-    ben_outflow = ben_outflow - hash["amount"]
-  elsif hash["from_user"] = "brian"
-    brian_outflow = brian_outflow - hash["amount"]
-  elsif hash["from_user"] = "evan"
-    evan_outflow = evan_outflow - hash["amount"]
-  elsif hash["from_user"] = "anthony"
-    anthony_outflow = anthony_outflow - hash["amount"]
+# Loop calculating subtractions to wallet
+loop do 
+
+  # Stop loop once we've gone through all the hashes
+  if blockchain_index == blockchain.count
+    break
   end
+
+  case
+    when blockchain[blockchain_index]["from_user"] == "ben"
+      ben_total = ben_total - blockchain[blockchain_index]["amount"]
+    when blockchain[blockchain_index]["from_user"] == "brian"
+      brian_total = brian_total - blockchain[blockchain_index]["amount"]
+    when blockchain[blockchain_index]["from_user"] == "evan"
+      evan_total= evan_total - blockchain[blockchain_index]["amount"]
+    when blockchain[blockchain_index]["from_user"] == "anthony"
+      anthony_total = anthony_total - blockchain[blockchain_index]["amount"]
+  end
+
+  blockchain_index = blockchain_index + 1
+
 end
 
-# Get total wallet amounts
-ben_total = ben_inflow + ben_outflow
-brian_total = brian_inflow + brian_outflow
-evan_total = evan_inflow + evan_outflow
-anthony_total = anthony_inflow + anthony_outflow
-
+# Print total balances
 puts "Ben's KelloggCoin balance is #{ben_total}"
 puts "Brian's KelloggCoin balance is #{brian_total}"
 puts "Evan's KelloggCoin balance is #{evan_total}"
